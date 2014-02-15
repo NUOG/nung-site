5cca4378e022df5fd80b27cfef7c8280f173b67a
root <root@hosting.(none)>
2014-02-15 06:05:06 -0500

    Add Power Menu editor

---
+++ sites/all/themes/nung/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * template.php
+ */
+
+/* Change long language name to short */
+/*
+function nung_links__locale_block(&$vars) {
+  foreach($vars['links'] as $language => $langInfo) {
+        $vars['links'][$language]['title'] = $vars['links'][$language]['language']->language;
+  }
+  $content = theme_links($vars);
+  return $content;
+}
+*/
+
+
